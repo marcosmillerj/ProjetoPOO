c572fe4a07467a971332bea59e0f3698b38ebb4e
Marcos Miller <marcos_miller@M.Miller>
2024-10-22 23:27:08 -0300

    FIRST COMMIT

---
+++ src/main/java/com/mycompany/miller_projetopoo/Miller_PROJETOPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miller_projetopoo;
+
+/**
+ *
+ * @author marcos_miller
+ */
+public class Miller_PROJETOPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
